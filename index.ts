71371106b0a78906260e8a9be2e3f05ac8e5ab4e
COHL7500 <cohl7500@gmail.com>
2023-08-02 12:31:37 +0200

    feat: add some stuff idk

--- index.ts
+++ index.ts
@@ -1,7 +1,93 @@
-const w = 500;
-const h = 500;
-const r = 20;
-const svg = d3.select("#canvas")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h)
+const canvas = d3.select("#canvas");
+
+const maxPos: number = 800;
+const minPos: number = 20;
+
+type Animal = {
+    name: string,
+    type: string,
+    temp: number[],
+    img: string,
+    text?: string
+    x: number,
+    y: number
+};
+
+let animalMap = new Map<string, Animal>;
+
+d3.csv("dataset.csv").then(d => {
+
+    const tempCol = d.columns.slice(2);
+
+    d.map((d) => {
+
+        const animal: Animal = {
+                name: d.name,
+                type: d.type,
+                temp: tempCol.map((col) => +d[col]),
+                img: "img/" + d.name + ".jpg",
+                x: (Math.floor(Math.random() * (800 - 10 + 1) + 10)),
+                y: (Math.floor(Math.random() * (800 - 10 + 1) + 10)),
+            };
+
+        animalMap.set(d.name, animal);
+
+        addAnimal(animal);
+    });
+
+    console.log(animalMap)
+})
+
+const addAnimal = (animal: Animal) => {
+    canvas.append("img")
+        .attr("src", animal.img)
+        .style("width", "10%")
+        .style("height", "auto")
+        .style("transform", `translate(${animal.x}px, ${animal.y}px)`)
+
+};
+
+// info bar bottom
+
+canvas
+    .append("div")
+    .attr("class", "bottom-info-bar")
+    .append("p")
+    .text("2023")
+    .append("p")
+    .text("0C")
+
+
+/*
+const w = 1000;
+const h = 1000;
+var canvas = d3
+  .select('#canvas')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
+  .style('background-color', 'black');
+var animalData = [];
+d3.csv('climatedata.csv').then(function (data) {
+  animalData = data;
+  draw();
+  console.log(animalData);
+});
+var trigger = "one";
+function draw() {
+  canvas
+    .selectAll('circle')
+    .data(animalData)
+    .join('circle')
+    .attr('cx', function (d, i) {
+      console.log(d);
+      return i * 30;
+    })
+    .attr('cy', h / 2)
+    .attr('r', function (d) {
+      console.log(d)
+      return d.one;
+    })
+    .attr('fill', 'white');
+}
+ */
