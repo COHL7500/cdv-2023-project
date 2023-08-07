c2a8f1ed221fe7148b97a18aa4c650e0fc700dc0
Mhttt <74661052+Mhttt@users.noreply.github.com>
2023-08-07 14:22:20 +0200

    added graveyard, changed transition duration

--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
-const canvas = d3.select("#canvas");
+const canvas = d3.select('#canvas');
 
 let currYear: number = 2020;
 let currTemp: number = 0;
 
 type Animal = {
-    name: string,
-    type: string,
-    temp: number[],
-    img: string,
-    text?: string
-    x: number,
-    y: number
+  name: string;
+  type: string;
+  temp: number[];
+  img: string;
+  text?: string;
+  extinctDegree: number;
+  x: number;
+  y: number;
 };
 
-let animalMap = new Map<string, Animal>;
+let animalMap = new Map<string, Animal>();
 
-d3.csv("dataset.csv").then(d => {
-
-    const tempCol = d.columns.slice(2);
+d3.csv('dataset.csv').then((d) => {
+  const tempCol = d.columns.slice(2);
 
   d.map((d) => {
     const animal: Animal = {
-        name: d.name,
-        type: d.type,
-        temp: tempCol.map((col) => +d[col]),
-        img: 'img/' + d.name + '.png',
-        x: parseInt(d.x),
-        y: parseInt(d.y),
-        text: d.text,
+      name: d.name,
+      type: d.type,
+      temp: tempCol.map((col) => +d[col]),
+      img: 'img/' + d.name + '.png',
+      x: parseInt(d.x),
+      y: parseInt(d.y),
+      extinctDegree: parseFloat(d.extinctDegree),
+      text: d.text,
     };
 
     animalMap.set(d.name, animal);
@@ -41,12 +42,11 @@ d3.csv("dataset.csv").then(d => {
     d3.select('#start-container').remove();
     startFade(animalMap);
     const infoUpdateInterval = setInterval(() => {
-        if (currYear == 2500) {
-            clearInterval(infoUpdateInterval);
-        }
+      if (currYear == 2600) {
+        clearInterval(infoUpdateInterval);
+      }
 
-        updateInfo();
-
+      updateInfo();
     }, 75); // 36 seconds to go from 0C to 6C = 75 milliseconds per year
   });
 });
@@ -56,7 +56,7 @@ const addAnimal = (animal: Animal) => {
     .append('img')
     .attr('src', animal.img)
     .attr('id', animal.name)
-    .style('max-width', "150px")
+    .style('max-width', '150px')
     .style('height', 'auto')
     .style('top', `${animal.y}px`)
     .style('left', `${animal.x}px`)
@@ -67,79 +67,99 @@ const addAnimal = (animal: Animal) => {
   animalElement.on('mousemove', (event) => onMouseMove(event, animal));
 };
 
+const xScale = d3.scaleLinear().domain([0, 6]).range([6000, 36000]);
+
 function startFade(animalMap: Map<string, Animal>) {
   animalMap.forEach((animal) => {
+    const animalElement = d3.select('#' + animal.name);
+    let addToGrave = false;
+    animalElement
+      .transition()
+      .duration(xScale(animal.extinctDegree))
+      .tween('fade', () => {
+        const interpolate = d3.interpolate(1, 1 - animal.temp[12] / 100);
+        return (t) => {
+          if (
+            Number(((interpolate(t) * 100) / 100).toFixed(2)) == 0.02 &&
+            !addToGrave
+          ) {
+            addToGrave = true;
+            graveyard
+              .append('img')
+              .attr('src', animal.img)
+              .attr('id', `${animal.name}-grave`)
+              .style('max-width', '20px');
+          }
+          animalElement.style(
+            'opacity',
+            Number(((interpolate(t) * 100) / 100).toFixed(2))
+          );
+        };
+      });
 
-      const animalElement = d3.select('#' + animal.name);
-
-      animalElement
-          .transition()
-          .duration(36000)
-          .tween('fade', () => {
-              const interpolate = d3.interpolate(1, 1 - animal.temp[12] / 100);
-              return (t) => animalElement.style('opacity', interpolate(t));
-          });
-
-      /*
-    let startIndex = 1;
-    for (let i = startIndex; i <= startIndex + 11; i++) {
-      d3.select('#' + animal.name)
-        .transition()
-        .duration(36000)
-        .style('opacity', 1 - animal.temp[i] / 100);
-    }
-
-       */
+    // let startIndex = 1;
+    // for (let i = startIndex; i <= startIndex + 11; i++) {
+    //   d3.select('#' + animal.name)
+    //     .transition()
+    //     .duration(36000)
+    //     .style('opacity', 1 - animal.temp[i] / 100);
+    // }
   });
 }
 
-const tooltip = d3.select("#canvas")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-
+const tooltip = d3
+  .select('#canvas')
+  .append('div')
+  .style('opacity', 0)
+  .attr('class', 'tooltip');
 
 // tooltip
-const onMouseOver = function(d: Animal) {
-    tooltip.style("opacity", d3.select('#' + d.name).style('opacity'))
-}
+const onMouseOver = function (d: Animal) {
+  tooltip.style('opacity', d3.select('#' + d.name).style('opacity'));
+};
 
 const onMouseMove = function (event: any, d: Animal) {
-    tooltip
-        .html(`${d.text}`)
-        .style("left", event.pageX + 10 + "px")
-        .style("top", event.pageY + 10 + "px")
-}
+  tooltip
+    .html(`${d.text}`)
+    .style('left', event.pageX + 10 + 'px')
+    .style('top', event.pageY + 10 + 'px');
+};
 
-const onMouseLeave = function() {
-    tooltip.style("opacity", 0)
-}
+const onMouseLeave = function () {
+  tooltip.style('opacity', 0);
+};
+
+// graveyard
+const graveyardContainer = canvas
+  .append('div')
+  .attr('class', 'graveyard-container');
+const graveyardTitle = graveyardContainer
+  .append('h3')
+  .attr('class', 'graveyard-title')
+  .text('The following animals are now extinct....');
+const graveyard = graveyardContainer.append('div').attr('class', 'graveyard');
 
 // info bar bottom
-const bottomInfoBar = canvas
-    .append("div")
-    .attr("class", "bottom-info-bar");
+const bottomInfoBar = canvas.append('div').attr('class', 'bottom-info-bar');
 
-const infoContainer = bottomInfoBar.append("div")
-    .attr("class", "info-container");
+const infoContainer = bottomInfoBar
+  .append('div')
+  .attr('class', 'info-container');
 
-infoContainer
-    .append("p")
-    .attr("id", "yearInfo")
-    .text(currYear.toString());
+infoContainer.append('p').attr('id', 'yearInfo').text(currYear.toString());
 
 infoContainer
-    .append("p")
-    .attr("id", "tempInfo")
-    .text(currTemp + "C");
+  .append('p')
+  .attr('id', 'tempInfo')
+  .text(currTemp + 'C');
 
 function calcTemp(year) {
-    return -146.99 + (19.46 * Math.log(year));
+  return -146.99 + 19.46 * Math.log(year);
 }
 
 function updateInfo() {
-    d3.select("#yearInfo").text(currYear++);
-    d3.select("#tempInfo").text(calcTemp(currYear).toFixed(1) + "C");
+  d3.select('#yearInfo').text(currYear++);
+  d3.select('#tempInfo').text(calcTemp(currYear).toFixed(1) + 'C');
 }
 
 /*
