618b7d5f7963f53650ea8b322299688800b8d78d
Mhttt <74661052+Mhttt@users.noreply.github.com>
2023-08-07 15:06:53 +0200

    working smoke

--- index.ts
+++ index.ts
@@ -21,14 +21,14 @@ d3.csv('dataset.csv').then((d) => {
 
   d.map((d) => {
     const animal: Animal = {
-        name: d.name,
-        type: d.type,
-        temp: tempCol.map((col) => +d[col]),
-        img: 'img/' + d.type + '/' + d.name + '.png',
-        x: parseInt(d.x),
-        y: parseInt(d.y),
-        extinctDegree: parseFloat(d.extinctDegree),
-        text: d.text,
+      name: d.name,
+      type: d.type,
+      temp: tempCol.map((col) => +d[col]),
+      img: 'img/' + d.type + '/' + d.name + '.png',
+      x: parseInt(d.x),
+      y: parseInt(d.y),
+      extinctDegree: parseFloat(d.extinctDegree),
+      text: d.text,
     };
 
     animalMap.set(d.name, animal);
@@ -88,6 +88,7 @@ function startFade(animalMap: Map<string, Animal>) {
               .append('img')
               .attr('src', animal.img)
               .attr('id', `${animal.name}-grave`)
+              .style('margin', '4px')
               .style('max-width', '20px');
           }
           animalElement.style(
@@ -146,12 +147,107 @@ infoContainer
   .text(currTemp + 'C');
 
 function calcTemp(year: number) {
-    return -146.99 + (19.46 * Math.log(year));
+  return -146.99 + 19.46 * Math.log(year);
 }
 
 function updateInfo() {
   d3.select('#yearInfo').text(currYear++);
   d3.select('#tempInfo').text(calcTemp(currYear).toFixed(1) + 'C');
+}
+
+class Particle {
+  object: any; // Replace 'any' with the actual type of your object
+  x: number;
+  y: number;
+  scale: number;
+  duration: number;
+  grow: number;
+  velX: number;
+  maxVelY: number;
+  minVelY: number;
+  velY: number;
+  count: number;
+  id: string;
+
+  constructor(object, count, x, y, size) {
+    this.object = object;
+    this.x = x;
+    this.y = y + size * Math.random();
+    this.scale = parseFloat((3 / 10).toString()) * size;
+    this.duration = Math.random() * 10000;
+    this.grow = this.scale + size * Math.random();
+    this.velX =
+      (Math.random() < 0.5 ? -1 : 1) * parseFloat((1 / 2).toString()) * size;
+    this.maxVelY = size / 2;
+    this.minVelY = 3 * size;
+    this.velY = size * 3 * Math.random() + size * 2;
+    this.count = count;
+    this.id = `smoke-${this.object.id}-${this.count}`;
+
+    this.emit();
+  }
+
+  emit() {
+    d3.select(this.object)
+      .append('circle')
+      .attr('id', `smoke-${this.object.id}-${this.count}`)
+      .attr('class', 'particle')
+      .attr('cx', this.x)
+      .attr('cy', this.y)
+      .attr('r', this.scale)
+      .attr('fill', '#000000')
+      .attr('opacity', '0.5')
+      .transition()
+      .duration(this.duration)
+      .attr('cy', this.y - this.velY)
+      .attr('cx', this.x + this.velX)
+      .attr('r', this.grow)
+      .attr('opacity', '0')
+      .on('end', () => {
+        document
+          .getElementById(`smoke-${this.object.id}-${this.count}`)
+          .remove();
+      });
+  }
+}
+class Smoke {
+  id: number;
+  particles: Particle[];
+  limit: number;
+  step: number;
+  created: number;
+  interval: number;
+
+  constructor(id: number = 1) {
+    this.id = id;
+    this.particles = [];
+    this.limit = 50; // Max number of particles.
+    this.step = 50; // Max particles created per interval.
+    this.created = 0; // A incrementing numeric id for each particle.
+    this.interval = 200; // How often to add new particles.
+
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    setInterval(() => {
+      const filter = this.particles.filter((p) => {
+        return document.getElementById(p.id) !== null;
+      });
+
+      this.particles = filter;
+      let currentStep = 0;
+
+      while (this.particles.length < this.limit && currentStep < this.step) {
+        const point = document.querySelector(`#smoke`);
+        if (point instanceof Element) {
+          this.particles.push(new Particle(point, this.created, 0, -15, 25));
+          this.created += 1;
+          currentStep += 1;
+        }
+      }
+    }, this.interval);
+  }
 }
 
 /*
