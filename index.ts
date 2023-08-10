91b540b64c8e1f649537c029c4a00586c25afab4
Mhttt <74661052+Mhttt@users.noreply.github.com>
2023-08-10 13:00:12 +0200

    fix reset

--- index.ts
+++ index.ts
@@ -368,6 +368,7 @@ d3.select('#reset-button').on('click', () => {
   const url = new URL(window.location.href);
   url.searchParams.set('hideModal', 'true');
   window.location.href = url.toString();
+  window.location.reload();
 
   // You can also add other actions here that you want to perform before the page reloads
 });
